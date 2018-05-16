b1d09afc3d3b565a0d9371d885a28755c42cae87
Claire Juramy-Gilles <juramy@lpnhe.in2p3.fr>
2018-05-16 16:12:11 +0200

    More timing tests for ITL modified raft

--- python/diffraftscope.py
+++ python/diffraftscope.py
@@ -6,110 +6,94 @@ import numpy as np
 from matplotlib import pyplot as plt
 import multiscope
 
-datadir = '/Users/nayman/Documents/REB/TS8/RTM8/RDscans'
+datadir = '/Users/nayman/Documents/REB/TS8/'
 
-seqfile = 'RTM1/TS8_ITL_2s_newflush_v2.seq'
+#seqfile = 'RTM1/TS8_ITL_2s_newflush_v2.seq'
 
 #listlabels = ["Baseline", "BSS=0", "REB0"]
-listlabels = ["RD=13", "RD=14"]
+listlabels = ["R50", "R200"]
 
 #listscans = ["rtm8scanmodetm1/00_rtm8_tm_1_bias.fits",
 #            "specscans/BSS0/00_bias2.fits",
 #             "specscans/REB0/00_bias2.fits"]
-listscans = ["00_tm_bias_2.fits", "00_RD14_tm_bias_2.fits"]
+listscans = ["RTM10/RTM10-scan-mode-images/00_TS8_ITL_clockcross90_bias_scan_tm.fits",
+             "RTM10-R200/rtm10-rebuilt-scans/00_TS8_ITL_clockcross90_bias_scan_tm_2.fits"]
 
-fig, axes = plt.subplots(nrows = 3, ncols = 3, figsize=(15, 12))
+fig, axes = plt.subplots(nrows = 3, ncols = 3, figsize=(17, 12))
 
 color_idx = [plt.cm.jet(i) for i in np.linspace(0, 1, 16)]
 
 # plot baseline on first row
 raftarrays, seglist = multiscope.get_scandata_raft(listscans[0], datadir)
-
-for num in range(3):
-    tmscope = raftarrays[num * 4]
-    ax = axes[0, num  % 3 ]
-
-    # single CCD plot
-    for c in range(16):
-        tmchan = tmscope[c].mean(axis=0)
-        ax.plot(tmchan, label=c, color=color_idx[c])
-
-        ax.set_xlim(0, 255)
-        ax.set_xticks(np.arange(0, 256, 32))
-        ax.set_title(seglist[num * 4])
-
-        if num%3 == 0:
-            ax.set_ylabel('RD13 scan (ADU)')
-        ax.grid(True)
-
-
 raftarrays2, seglist = multiscope.get_scandata_raft(listscans[1], datadir)
 
-for num in range(3):
-    tmscope = raftarrays2[num * 4]
-    ax = axes[1, num  % 3 ]
 
-    # single CCD plot
-    for c in range(16):
-        tmchan = tmscope[c].mean(axis=0)
-        ax.plot(tmchan, label=c, color=color_idx[c])
+for j in range(3):
 
-        ax.set_xlim(0, 255)
-        ax.set_xticks(np.arange(0, 256, 32))
-        ax.set_title(seglist[num * 4])
+    for num in range(3):
+        tmscope = raftarrays[num * 3 + j]
+        ax = axes[0, num]
 
-        if num%3 == 0:
-            ax.set_ylabel('RD14 scan (ADU)')
-        ax.grid(True)
+        # single CCD plot
+        for c in range(16):
+            tmchan = tmscope[c].mean(axis=0)
+            ax.plot(tmchan, color=color_idx[c])
 
-# subtract baseline from second scan
-for num in range(3):
-    tmscope = raftarrays[num * 4]
-    tmscope2 = raftarrays2[num * 4]
-    ax = axes[2, num  % 3 ]
+            ax.set_xlim(0, 255)
+            ax.set_xticks(np.arange(0, 256, 32))
+            ax.set_title(seglist[num * 3 + j])
 
-    # single CCD plot
-    for c in range(16):
-        tmchan = tmscope2[c].mean(axis=0) - tmscope[c].mean(axis=0)
-        ax.plot(tmchan, color=color_idx[c])
+            if num == 0:
+                ax.set_ylabel('R50 scan (ADU)')
+            ax.grid(True)
 
-        ax.set_xlim(0, 255)
-        ax.set_xticks(np.arange(0, 256, 32))
-        ax.set_title(seglist[num * 4])
 
-        if num%3 == 0:
-            ax.set_ylabel('RD14 - RD13 (ADU)')
-        if num/3 == 2:
+
+    for num in range(3):
+        tmscope = raftarrays2[num * 3 + j]
+        ax = axes[1, num ]
+
+        # single CCD plot
+        for c in range(16):
+            tmchan = tmscope[c].mean(axis=0)
+            ax.plot(tmchan, color=color_idx[c])
+
+            ax.set_xlim(0, 255)
+            ax.set_xticks(np.arange(0, 256, 32))
+            ax.set_title(seglist[num * 3 + j])
+
+            if num == 0:
+                ax.set_ylabel('R200 scan (ADU)')
+            ax.grid(True)
+
+    # subtract first scan from second scan
+    for num in range(3):
+        tmscope = raftarrays[num * 3 + j]
+        tmscope2 = raftarrays2[num * 3 + j]
+        ax = axes[2, num]
+
+        # single CCD plot
+        for c in range(16):
+            tmchan = tmscope2[c].mean(axis=0) - tmscope[c].mean(axis=0)
+            ax.plot(tmchan, label=c, color=color_idx[c])
+
+            ax.set_xlim(0, 255)
+            ax.set_xticks(np.arange(0, 256, 32))
+            ax.set_title(seglist[num * 3 + j])
+            ax.set_ylim(-5000, 5000)
+
+            if num == 0:
+                ax.set_ylabel('RD200 - RD50 (ADU)')
             ax.set_xlabel('Time increment (10 ns)')
-        ax.grid(True)
-
-# subtract baseline from REB0
-#raftarrays3, seglist = multiscope.get_scandata_raft(listscans[2], datadir)
-#
-#for num,tmscope,tmscope2 in zip(range(3), raftarrays[:3], raftarrays3[:3]):
-#    ax = axes[2, num  % 3 ]
-
-    # single CCD plot
-#    for c in range(16):
-#        tmchan = tmscope2[c].mean(axis=0) - tmscope[c].mean(axis=0)
-#        ax.plot(tmchan, color=color_idx[c])
-#
-#        ax.set_xlim(0, 255)
-#        ax.set_xticks(np.arange(0, 256, 32))
-#        ax.set_title(seglist[num])
-#
-#        if num%3 == 0:
-#            ax.set_ylabel('REB0-Baseline (ADU)')
-#        ax.set_xlabel('Time increment (10 ns)')
-#        ax.grid(True)
+            ax.grid(True)
 
 
-ax.legend(bbox_to_anchor=(1.05, 0), loc='lower left', borderaxespad=0.)
+    ax.legend(bbox_to_anchor=(1.05, 0), loc='lower left', borderaxespad=0.)
 
-plt.suptitle("Difference between scans", fontsize='x-large')
+    plt.suptitle("Difference between scans", fontsize='x-large')
 
-plt.savefig(os.path.join(datadir, "diffraftscope.png"))
-plt.show()
+    plt.savefig(os.path.join(datadir, "diffraftscope%d.png" % j))
+    plt.show()
 
 
 
--- python/scriptraftscope.py
+++ python/scriptraftscope.py
@@ -2,44 +2,44 @@ import scope
 import raftstats
 import multiscope
 
-rtm = "RTM8b"
+rtm = "RTM10-R200"
 c = "S3"
 
 #datadir = "/Users/nayman/Documents/REB/TS8/%s/camel_v3" % rtm
 #datadir = "/Users/nayman/Documents/REB/TS8/%s/rtm9scans1" % rtm
 #datadir = '/Users/nayman/Documents/REB/TS8/%s/singleclockscans/single%s' % (rtm, c)
-datadir = '/Users/nayman/Documents/REB/TS8/RTM8bis/'
 #datadir = '/Users/nayman/Documents/REB/TS8/RTM8/superscan'
 #datadir = "/Users/nayman/Documents/REB/TS8/%s/RGhigh" % rtm
-#datadir = "/Users/nayman/Documents/REB/TS8/%s/injectRD" % rtm
+datadir = "/Users/nayman/Documents/REB/TS8/%s/rtm10-rebuilt-scans" % rtm
 
 #seqfile = 'TS8_ITL_ResetFirst_CJ_20170321_mod3.seq'
-seqfile = 'ITL/TS8_ITL_2s_newflush_v4.seq'
+#seqfile = 'ITL/TS8_ITL_2s_newflush_v4.seq'
 #seqfile = 'RTM8/TS8_ITL_longS1_scan.seq'
 #seqfile = "E2V/seq-e2v-RGhigh.seq"
-#seqfile = "RTM10/TS8_ITL_2s_camel_v3.seq"
+seqfile = "RTM10/TS8_ITL_clockcross90.seq"
 
-tmbasefile = "rtm8run2scan1/00_rtm8_tm_scan_2.fits"
+#tmbasefile = "rtm8run2scan1/00_rtm8_tm_scan_2.fits"
 #tmbasefile = "00_scan_tm_TS8_ITL_injectRD_fix_2.fits"
 #tmbasefile = "00_scan_tm_TS8_ITL_singleS3.fits"
 #tmbasefile = "00_scan_tm_TS8_ITL_RGhigh_longS1_fix.fits"
 #tmbasefile = "00_RTM10_scan_tm_TS8_ITL_RGhigh_allS.fits"
 #tmbasefile = "basescan/00_TS8_ITL_longS1_scan_tm_2.fits"
 #tmbasefile = "00_TS8_ITL_RGhigh_longS1_fix_bias_scan_dsi.fits"
-#tmbasefile = "00_TS8_ITL_clockcross90_bias_scan_tm.fits"
+tmbasefile = "00_TS8_ITL_clockcross90_bias_scan_tm_2.fits"
 #tmbasefile = "00_bias2.fits"
 #tmbasefile = "00_seq-e2v-RGhigh_scan_tm_2.fits"
 #tmbasefile = "00_2s_camel_v3_scan_tm_1.fits"
 #dsibasefile = "00_2s_camel_v3_scan_dsi_1.fits"
+dsibasefile = "00_TS8_ITL_clockcross90_bias_scan_dsi_2.fits"
 
 # returns tuple: (list of files, list of segments)
-#l = raftstats.get_fits_raft(inputfile=tmbasefile, datadir=datadir)
-#ldsi = raftstats.get_fits_raft(inputfile=dsibasefile, datadir=datadir)
+l = raftstats.get_fits_raft(inputfile=tmbasefile, datadir=datadir)
+ldsi = raftstats.get_fits_raft(inputfile=dsibasefile, datadir=datadir)
 
 
 #---- Scan display for all raft channels
 
-multiscope.raft_display_allchans(tmbasefile, datadir, 'RTM8 reconstructed')
+#multiscope.raft_display_allchans(tmbasefile, datadir, 'RTM-13 reconstructed')
 
 #---- Combined display of single channel with clock sequences
 
@@ -99,15 +99,16 @@ multiscope.raft_display_allchans(tmbasefile, datadir, 'RTM8 reconstructed')
 
 #---- Comparing scans channel per channel for a single CCD
 
-# Mixing raw and fits files does not work now
-#listlabels = ["Baseline", "BSS=0", "REB0"]
-#listlabels = [s for s in ["%d%d" % (i, j) for i in range(3) for j in range(3)]]
+datadir = "/Users/nayman/Documents/REB/TS8"
 
-#listscans = ["rtm8scanmodetm1/01_rtm8_tm_1_bias.fits",
-#             "specscans/BSS0/01_bias2.fits",
-#             "specscans/REB0/01_bias2.fits"]
+#listlabels = [s for s in ["%d%d" % (i, j) for i in range(3) for j in range(3)]]
+listlabels = ["R50", "R200"]
+
 #listscans = ["rtm8scanmodetm1/%s_rtm8_tm_1_bias.fits" % s for s in listlabels]
+listscans =["RTM10/RTM10-scan-mode-images/%s_TS8_ITL_clockcross90_bias_scan_tm.fits",
+             "RTM10-R200/rtm10-rebuilt-scans/%s_TS8_ITL_clockcross90_bias_scan_tm_2.fits"]
 
 #scope.compare_scope_display(listscans, listlabels, datadir, title='Channels of S01', diff=False)
 
-
+for s in ["%d%d" % (i, j) for i in range(3) for j in range(3)]:
+    scope.compare_scope_display([listscans[0] % s, listscans[1] % s], listlabels, datadir, title='Channels of S%s' % s, diff=False)
